ac2afe73802104d9a17895b4a005c2771e919cff
Sam Bryant <samuellbryant@msn.com>
2018-06-14 21:07:03 -0400

    Changes in the main program

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace BankProgram
                 Account checking = new Checking("BankName", "bob", 200);
                 checking.deposit(200);
                 checking.withdraw(10);
-                ///Account account = new Account("hthed55", "tdhwsrr", 200);
+                
 
                 Investment inv = new Individual("Bank", "bob", 600);
                 inv.deposit(1000);
@@ -21,7 +21,7 @@ namespace BankProgram
                 Console.WriteLine(checking);
                 Console.WriteLine(inv);
                 Console.WriteLine(cap);
-                Console.WriteLine("My first name {0} and my last name {1}", "Sam", "Bryant");
+               
                 Console.WriteLine("Press any key to continue");
                 Console.ReadKey();
             }
@@ -31,7 +31,7 @@ namespace BankProgram
             }
             catch (Exception e)
             {
-                Console.WriteLine("Something happen {0}, {1}", "hi" ,"bye" );
+                Console.WriteLine("Something happened!" );
             }
         }
     }
